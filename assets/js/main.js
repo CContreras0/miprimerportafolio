d5bdd1a5b9b3d66fa34e37b232ea8776ed327409
Claudia <cn.contrerasa@gmail.com>
2024-08-01 22:06:17 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import bootstrap from 'bootstrap'
+
+//
+// Place any custom JS here
+//
